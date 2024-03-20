224a1464a3c1f45fe03c6969bdb0c17c4464f5b9
Carlos Garcia <calbertogarciaramirez123@gmail.com>
2024-03-20 15:28:02 -0500

    ejercicios terminados

--- Javascript16/ejercicio15.js
+++ Javascript16/ejercicio15.js
@@ -1,10 +1,14 @@
-function convertir(num) {
-    if (num < 0 || num > 5) {
-        console.error("NO pueden ser numeros mayores a 5 o menores a 0")
-    }  
-    const texto = ['cero', 'uno', 'dos', 'tres', 'cuatro', 'cinco'];
-    return texto[num];  
-    }
+// Crea una función que convierta números del 0 al 5 en su representación textual, por
+// ejemplo: “cero”, “uno”, “dos”, “tres” etc.
+
+function textual(num) {
+  if (num < 0 || num > 5) {
+    alert("El numero debe ser menor que 5 y mayor que 0")
+  } else {
+    let texto = ["cero", "uno", "dos", "tres", "cuatro", "cinco"]
+    return texto[num];
+  }
+}
 let numero = prompt("Ingrese el numero");
-let valor = convertir(numero)
-alert(valor)
+let texto = textual(numero)
+console.log(texto);
--- Javascript16/ejercicio9.js
+++ Javascript16/ejercicio9.js
@@ -1,13 +1,63 @@
 // • Genera un patrón de asteriscos en forma de pirámide
 
-function asteriscos(num_asteriscos) {
-  for (let i = 1; i <= num_asteriscos; i++) {
-    let asterisco = '';
+
+function piramide(numero) {
+
+  for (let i = 1; i <= numero; i++) {
+    let cantidad_Asteriscos = '';
     for (let f = 1; f <= i; f++) {
-      asterisco += '*';
+      cantidad_Asteriscos += "*";
     }
-    console.log(asterisco);
+    console.log(cantidad_Asteriscos);
+
   }
 }
-let numero = parseInt(prompt("Ingrese un número entero"));
-let cantidad = asteriscos(numero);
+let numero = parseInt(prompt("Ingrese el numero"));
+
+let patron = piramide(numero);
+console.log(patron);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function asteriscos(num_asteriscos) {
+//   for (let i = 1; i <= num_asteriscos; i++) {
+//     let asterisco = '';
+//     for (let f = 1; f <= i; f++) {
+//       asterisco += '*';
+//     }
+//     console.log(asterisco);
+//   }
+// }
+// let numero = parseInt(prompt("Ingrese un número entero"));
+// let cantidad = asteriscos(numero);
